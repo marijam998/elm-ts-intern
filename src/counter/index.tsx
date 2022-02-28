860f17ee39347fa58b2085c3cbb0057bb0607b7d
Marija Milicevic <marija.milicevic@saga.rs>
2022-02-28 16:00:49 +0100

    Inicijalni commit counter

--- src/counter/index.tsx
+++ src/counter/index.tsx
@@ -1,19 +1,28 @@
-import { Html }  from 'elm-ts/lib/React';
+import { Html } from 'elm-ts/lib/React';
 import * as Cmd from 'elm-ts/lib/Cmd'
 
 // --- Model
-export type Model = number
+export type Model = {
+  counter: number,
+  name: string
+}
 
-export const init: [Model, Cmd.Cmd<Msg>] = [0, Cmd.none]
+export const init: [Model, Cmd.Cmd<Msg>] = [{ counter: 0, name: '' }, Cmd.none]
 
 // --- Messages
-export type Msg = { type: 'Increment' }
+export type Msg = { type: 'Increment' } | { type: 'Decrement' } | { type: 'Reset' } | { type: 'OnChangeName', value: string }
 
 // --- Update
 export const update = (msg: Msg, model: Model): [Model, Cmd.Cmd<Msg>] => {
   switch (msg.type) {
     case 'Increment':
-      return [model + 1, Cmd.none]
+      return [{ ...model, counter: model.counter + 1 }, Cmd.none]
+    case 'Decrement':
+      return [{ ...model, counter: model.counter - 1 }, Cmd.none]
+    case 'Reset':
+      return [{ ...model, counter: 0 }, Cmd.none]
+    case 'OnChangeName':
+      return [{ ...model, name: msg.value }, Cmd.none]
   }
 }
 
@@ -21,8 +30,11 @@ export const update = (msg: Msg, model: Model): [Model, Cmd.Cmd<Msg>] => {
 export const view = (model: Model): Html<Msg> => {
   return dispatch => (
     <div>
-      Count: {model}
+      Count: {model.counter}
       <button onClick={() => dispatch({ type: 'Increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'Decrement' })}>-</button>
+      <button onClick={() => dispatch({ type: 'Reset' })}>Reset</button>
+      <input value={model.name} type="text" onChange={(e) => dispatch({ type: 'OnChangeName', value: e.target.value })} />
     </div>
   )
 }
